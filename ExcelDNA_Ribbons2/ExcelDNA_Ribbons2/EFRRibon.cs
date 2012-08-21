f9e3b7bebac1c2b3a773b970c08f8d0b27d59f31
Zhenyu Lu <luzhenyu1981@gmail.com>
2012-08-20 23:19:41 -0700

    A test project for both GitHub and ExcelDNA.
    A test project for learning GitHub. An ExcelDNA project has been added.

---
+++ ExcelDNA_Ribbons2/ExcelDNA_Ribbons2/EFRRibon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExcelDna.Integration.CustomUI;
+using ExcelDna.Logging;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Xml;
+
+namespace ExcelDNA_Ribbons2
+{
+    [ComVisible(true)]
+    public class EFRRibon : ExcelRibbon
+    {
+        public override string GetCustomUI(string uiName)
+        {
+            System.Windows.Forms.MessageBox.Show("HEY");
+            XmlDocument doc = new XmlDocument();
+            doc.Load(Assembly.GetExecutingAssembly().GetManifestResourceStream("ExcelDNA_Ribbons2.Resources.Ribbon.xml"));
+            return doc.InnerXml;
+        }
+
+        public void OnButtonPressed(IRibbonControl control)
+        {
+            MessageBox.Show("This is a test.","Information",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        public System.Drawing.Image GetRibbonControlImage(IRibbonControl control)
+        {
+            MessageBox.Show("GetRibbonControlImage");
+            return ExcelDNA_Ribbons2.Resources.brymck_48;
+        }
+    }
+}
